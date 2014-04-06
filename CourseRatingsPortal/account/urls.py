5aecd44de46dc62b67601eb4b49a364bc0ec2efd
Lucien Knechtli <lknechtli@gmail.com>
2014-04-06 16:05:57 -0400

    added account urls and placeholder views

--- CourseRatingsPortal/account/urls.py
+++ CourseRatingsPortal/account/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponse
+from account import views
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+        url(r'^user/$', views.account_home),
+        url(r'^user/edit_profile/$', views.edit_profile),
+        url(r'^user/view/\w{0,50}', views.get_profile),
 )
 
--- CourseRatingsPortal/account/views.py
+++ CourseRatingsPortal/account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from account.models import UserProfile
 
 # Create your views here.
+
+
+def account_home(request):
+    return HttpResponse("Placeholder: User Home")
+
+def edit_profile(request):
+    return HttpResponse("Placeholder: Edit own profile")
+
+def get_profile(request, user):
+    return HttpResponse("Placeholder: View any user profile")
