1bf1323963b3058450fbdbaa7481f904de668b63
Jesse Bond <jjb315@lehigh.edu>
2014-04-09 21:08:21 -0400

    fixed names for urls

--- CourseRatingsPortal/search/urls.py
+++ CourseRatingsPortal/search/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 urlpatterns=patterns('',
         url(r'^$', views.index, name='index'),
-        url(r'professor/$', views.search_prof),
+        url(r'professor/$', views.search_prof, name='professors'),
         url(r'course/$', views.search_course, name='courses'),
-        url(r'professor/*', views.initiate_prof_search, name='professors'),
-        url(r'course/*',views.initiate_course_search),
+        url(r'professor/*', views.initiate_prof_search, name='professors_result'),
+        url(r'course/*',views.initiate_course_search, name='courses_result'),
 
 )
