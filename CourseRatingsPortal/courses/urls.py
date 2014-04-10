c3b99f06169debc4ffeef6d1180d6dad5fa29b62
Curtis Giddings <curtisgiddings@gmail.com>
2014-04-10 01:50:20 -0400

    Fixed course search

--- CourseRatingsPortal/courses/urls.py
+++ CourseRatingsPortal/courses/urls.py
@@ -7,7 +7,7 @@ urlpatterns=patterns('',
         url(r'^$', views.index),
         url(r'^(?P<university>\w+)/(?P<course>\w+)\$', views.get_course), #university, course_name
         url(r'^/professor/(?P<university>\w+)/(?P<professor>\w+)\$', views.get_professor),
-        url(r'^/professor/create\$', views.ProfessorCreate.as_view(), name='professor_create'),
+        url(r'^/professor/create', views.ProfessorCreate.as_view()),
         url(r'^/university/create\$', views.UniversityCreate.as_view(), name='university_create'),
         url(r'^/department/create\$', views.DepartmentCreate.as_view(), name='department_create'),
         url(r'^/course/create\$', views.CourseCreate.as_view(), name='course_create'),
